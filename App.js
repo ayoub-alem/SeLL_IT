242b24a0cca233ca6003e7c90cdfab8bf896b791
Ayoub Alem <ayoub.alem@uit.ac.ma>
2022-07-19 09:26:39 +0100

    Creating ListItem Component

--- App.js
+++ App.js
@@ -3,22 +3,8 @@ import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: '#f8f4f4',
-        padding: 20,
-        paddingTop: 70,
-        flex: 1,
-      }}
-    >
-      <Card
-        title='Red jacket for sale'
-        subTitle={'$100'}
-        image={require('./app/assets/jacket.jpg')}
-      ></Card>
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }

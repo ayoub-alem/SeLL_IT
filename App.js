78a437c59af05ee8cf492943725646ff6cc916e7
Ayoub Alem <ayoub.alem@uit.ac.ma>
2022-07-19 13:11:07 +0100

    Creating the Icon component

--- App.js
+++ App.js
@@ -5,7 +5,19 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import colors from './app/config/colors';
+import Icon from './app/components/Icon';
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon
+        name='email'
+        size={50}
+        backgroundColor='red'
+        iconColor='white'
+      ></Icon>
+    </Screen>
+  );
 }

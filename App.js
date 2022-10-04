8766253378618daa197ddc2199cba881fc1d0a93
Ayoub Alem <ayoub.alem@uit.ac.ma>
2022-10-04 10:54:00 +0100

    Refactoring the App file

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
-import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import Screen from './app/components/Screen';
@@ -16,18 +15,13 @@ import AppPicker from './app/components/AppPicker';
 import LoginScreen from './app/screens/LoginScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
-import ImageInput from './app/components/ImageInput';
-import ImageInputList from './app/components/ImageInputList';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
-import AppText from './app/components/AppText';
-import Constants from 'expo-constants';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
-import jwtDecode from 'jwt-decode';
 import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
@@ -35,15 +29,15 @@ export default function App() {
   const [user, setUser] = useState(null);
   const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (token) setUser(jwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
     setIsReady(true);
     await SplashScreen.hideAsync();
   };
 
   useEffect(() => {
-    restoreToken();
+    restoreUser();
   }, []);
 
   if (!isReady) return null;
